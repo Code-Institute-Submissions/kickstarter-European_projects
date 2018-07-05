56c8e9c74fab18846e65bb454914d37d2c2fed95
Giselle <gigi108@gmail.com>
2018-07-05 21:07:28 +0000

    testing graphs

--- graph.js
+++ graph.js
@@ -5,35 +5,61 @@ queue()
 function makeGraph(error, transactionsData) {
     let ndx = crossfilter(transactionsData);
 
-    let countryDim = ndx.dimension(dc.pluck("country_code"));
-
-    let pledgeByCountry = countryDim.group().reduceSum(dc.pluck("pledged"));
-
-
-    let pledgeCountryChart = dc.barChart("#pledgeCountryChart");
-
-    let selectCountryDim = ndx.dimension(dc.pluck("country"));
-    let selectCountryGroup = selectCountryDim.group();
-
-    let selectCountry = dc.selectMenu("#select-country")
-
-    selectCountry
-        .dimension(selectCountryDim)
-        .group(selectCountryGroup);
-
-
-    pledgeCountryChart
-        .width(600)
-        .height(400)
-        .margins({ top: 10, right: 50, bottom: 50, left: 70 })
-        .dimension(countryDim)
-        .group(pledgeByCountry)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .xAxisLabel("countryDim");
-    // .yAxis().ticks(5);
+    projectsByCountry(ndx);
+    country_selector(ndx)
+    status_selector(ndx);
+    status_balance(ndx);
 
 
     dc.renderAll();
 
+}
+
+function projectsByCountry(ndx) {
+    let countryDim = ndx.dimension(dc.pluck("country_code"));
+    let group = countryDim.group();
+
+    dc.pieChart("#projects-country")
+        .height(300)
+        .radius(100)
+        .innerRadius(15)
+        .dimension(countryDim)
+        .group(group);
+}
+
+function country_selector(ndx) {
+    let countryDim = ndx.dimension(dc.pluck("country"));
+    let group = countryDim.group();
+    
+    dc.selectMenu("#country_filter")
+        .dimension(countryDim)
+        .group(group);
+}
+
+function status_selector(ndx) {
+    let statusDim = ndx.dimension(dc.pluck("status"));
+    let group = statusDim.group();
+
+    dc.selectMenu("#status_filter")
+        .dimension(statusDim)
+        .group(group);
+
+}
+
+function status_balance(ndx) {
+    let statusDim = ndx.dimension(dc.pluck("status"));
+    let group = statusDim.group();
+
+    dc.barChart("#status-balance")
+        .width(400)
+        .height(300)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(statusDim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel("Status")
+        .yAxis().ticks(5);
 }
